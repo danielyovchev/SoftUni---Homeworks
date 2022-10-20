66235e84397015940332c359ed053fd728195355
danielyovchev <100419579+danielyovchev@users.noreply.github.com>
2022-10-20 22:23:23 +0300

    Add files via upload

--- JS - Advanced/Exam Preparation1/02. Garden/solution.js
+++ JS - Advanced/Exam Preparation1/02. Garden/solution.js
@@ -1,1 +1,36 @@
-//TODO
+class Garden {
+    constructor(spaceAvailable){
+        this.spaceAvailable = spaceAvailable;
+        this.plant = [];
+        this.storage = [];
+    }
+    addPlant(plantName, spaceRequired){
+        if(spaceRequired > this.spaceAvailable){
+            throw new Error("Not enough space in the garden.");
+        }
+        let plantObj = {
+            plantName: plantName,
+            spaceRequired: spaceRequired,
+            ripe: false,
+            quantity: 0
+        };
+        this.spaceAvailable-=spaceRequired;
+        this.plant.push(plantObj);
+        return `The ${plantName} has been successfully planted in the garden.`;
+    }
+    ripenPlant(plantName, quantity){
+        
+    }
+    harvestPlant(plantName){
+
+    }
+    generateReport(){
+
+    }
+}
+const myGarden = new Garden(250)
+
+console.log(myGarden.addPlant('apple', 20));
+console.log(myGarden.addPlant('orange', 200));
+console.log(myGarden.)
+//console.log(myGarden.addPlant('olive', 50));
